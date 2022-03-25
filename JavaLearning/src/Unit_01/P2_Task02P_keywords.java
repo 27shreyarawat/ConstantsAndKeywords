84c9bb4cc0065cd922c0cbaee5c037823cd68edf
shrey <shrey@LAPTOP-TO6UC9A8>
2022-03-25 13:15:18 +0530

    second commit

---
+++ JavaLearning/src/Unit_01/P2_Task02P_keywords.java
@@ -0,0 +1,22 @@
+package Unit_01;
+
+public class P2_Task02P_keywords {
+
+
+	int double = 10 //Syntax error on token "double", invalid VariableDeclaratorId
+			
+    int float() //Syntax error on tokens, delete these tokens
+    {
+		System.out.println( "hello");
+    }
+	ABC int = new ABC();//Syntax error on token "int", invalid VariableDeclaratorId
+}
+class ABC
+{
+	int p2()
+	{
+		System.out.println("ABC");
+	}
+	
+	 
+}
